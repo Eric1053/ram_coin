4286cbfd1b5ab995f39b27d6d826e85f3efffe25
Eric1053 <90023056+Eric1053@users.noreply.github.com>
2022-04-28 08:32:53 -0400

    Update deploy_ramcoin.sh

--- deploy_ramcoin.sh
+++ deploy_ramcoin.sh
@@ -4,18 +4,18 @@ namespaceStatus=$(kubectl get namespaces ramcoin -o json | jq .status.phase -r)
 
 if [ $namespaceStatus == "Active" ]
 then
-    echo "Namespace ramcoin exists, need to clean up"
-    kubectl delete namespaces ramcoin
+    echo "Namespace redTeam exists, need to clean up"
+    kubectl delete namespaces redTeam
 fi
 
-echo "Creating namespace ramcoin"
-kubectl create namespace ramcoin 
+echo "Creating namespace redTeam"
+kubectl create namespace redTeam 
  
 echo "Creating pods"
-kubectl create -f ramcoin.yaml --namespace ramcoin
+kubectl create -f ramcoin.yaml --namespace redTeam
 
 echo "Creating services"
-kubectl create -f ramcoin-service.yaml --namespace ramcoin
+kubectl create -f ramcoin-service.yaml --namespace redTeam
 
 
 kubectl get pods -n ramcoin
